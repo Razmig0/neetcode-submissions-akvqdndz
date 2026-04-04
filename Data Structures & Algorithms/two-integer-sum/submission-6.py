edd283ea557656c663a767a610cf226666d74564
Razmig_0 <thomassianrazmig@gmail.com>
2026-04-04 14:58:25 +0400

    Bulk sync: 19 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        t = target
+        for i in range(len(nums)):
+         for j in range(i+1, len(nums)):
+            n = nums[i]
+            g = nums[j]
+            if n + g == t:
+                return [i,j]
+        return False
+
---
+++ SQL/sql-customers-with-positive-revenue/submission-0.sql
@@ -0,0 +1,4 @@
+-- Write your query below
+SELECT customer_id
+From customers
+where year = 2020 AND revenue > 0 
